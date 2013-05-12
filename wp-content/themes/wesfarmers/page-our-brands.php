3abcd1b2672fa9c418707ba45a6ab8d6b7e009d2
Francis Park <francis@binzsolutions.co.nz>
2013-05-13 10:41:38 +1200

    Checking if logos have show_in_our_brands field set before displaying.

--- wp-content/themes/wesfarmers/page-our-brands.php
+++ wp-content/themes/wesfarmers/page-our-brands.php
@@ -6,16 +6,18 @@
         
         <section class="wrapper clearfix">
   <?php $counter = 0; ?>
-<?php $pods = new Pod('brand'); $pods->findRecords('name ASC', 12); ?>
+<?php $pods = new Pod('brand'); $pods->find(array('orderby' => 'post_date DESC')); //$pods->findRecords('name ASC', 12); ?>
             <article class="clearfix">
                 <?php the_content(); ?>
                 <ul id="brand-list" class="clearfix">
                     <?php while ($pods->fetchRecord()) : ?>
+                    <?php if ($pods->get_field('show_in_our_brands')) : ?>
                     <?php $logo = $pods->get_field('brand_logo'); ?>
                     <li>
                         <a href="<?php echo $pods->get_field('brand_website'); ?>" class="img" target="_blank"><img src="<?php echo $logo['0']['guid']; ?>" alt="<?php echo $pods->get_field('title'); ?>" /></a>
                         <p><?php echo $pods->get_field('brand_description'); ?></p>
                     </li>
+                    <?php endif; ?>
                     <?php endwhile; ?>
                 </ul>
             </article>
